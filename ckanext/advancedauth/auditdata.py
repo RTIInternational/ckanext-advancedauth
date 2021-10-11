e7d0d053d8af70c56d369ffd69e0d13a7db7c12b
Michael Long <michaellong@rti.org>
2021-10-11 12:18:41 -0400

    Add API endpoints for audit table

---
+++ ckanext/advancedauth/auditdata.py
@@ -0,0 +1,50 @@
+from ckan.common import request
+import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
+
+from .model import advancedauthAudit
+
+
+audit_table = Blueprint("audit_table", __name__)
+
+
+@audit_table.route("/audituser")
+def user_audit():
+  if toolkit.g.userobj.sysadmin:
+    user_id = request.params.get("userid", None)
+    rows = advancedauthAudit().get_all_actions_by_user(user_id)
+    actions = map(map_row_data, rows)
+    return {action["id"]: {k:v for k,v in action.items() if k != "id"} for action in actions}
+  return "No lookie. Only sysadmin"
+  
+
+@audit_table.route("/auditfile")
+def file_audit():
+  if toolkit.g.userobj.sysadmin:
+    resource_id = request.params.get("resourceid", None)
+    rows = advancedauthAudit().get_all_actions_by_file(resource_id)
+    actions = map(map_row_data, rows)
+    return {action["id"]: {k:v for k,v in action.items() if k != "id"} for action in actions}
+  return "No lookie. Only sysadmin"
+
+
+@audit_table.route("/auditdates")
+def date_audit():
+  if toolkit.g.userobj.sysadmin:
+    start_date = request.params.get("startdate", "2000-01-01")
+    end_date = request.params.get("enddate", "3000-12-12")
+    rows = advancedauthAudit().get_all_actions_by_date(start_date, end_date)
+    actions = map(map_row_data, rows)
+    return {action["id"]: {k:v for k,v in action.items() if k != "id"} for action in actions}
+  return "No lookie. Only sysadmin"
+
+
+
+def map_row_data(row):
+  return {
+    "id": row.id,
+    "user_id": row.user_id,
+    "action": row.action,
+    "package_id": row.package_id,
+    "resource_id": row.resource_id
+  }
