a44b48e63b844f7d3d21d4c722515d525d1f8344
Alex Harding <alexharding@rti.org>
2022-05-26 09:07:50 -0400

    adding hedge for completely missing validation

--- ckanext/advancedauth/validators.py
+++ ckanext/advancedauth/validators.py
@@ -3,7 +3,7 @@ from ckan.plugins.toolkit import Invalid
 # Raise error if uploaded file does not include an extension, or the extension is not allowed.
 def not_empty_string(key, flattened_data, errors, context):
     value = flattened_data.get(key, "")
-    if value.strip() == "":
+    if not value or value.strip() == "":
         raise Invalid("Field cannot be blank or consist of only spaces.")
 
 
