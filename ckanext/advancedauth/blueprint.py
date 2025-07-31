47ee77b033ca7a21740fd80968de278a745862db
Manuel Alvarado <manuelalvarado@rti.org>
2025-07-31 14:12:57 -0400

    remove password reset / register routes

--- ckanext/advancedauth/blueprint.py
+++ ckanext/advancedauth/blueprint.py
@@ -1,17 +1,12 @@
 import ckan.lib.base as base
 import ckan.lib.helpers as h
-import ckan.lib.mailer as mailer
 import ckan.lib.navl.dictization_functions as dictization_functions
 import ckan.logic as logic
-import ckan.model as model
 
-from ckan.common import _, g, request, current_user
-from ckan.views.user import RequestResetView, PerformResetView, EditView, RegisterView
+from ckan.common import _, g, request
+from ckan.views.user import EditView
 from .model import advancedauthExtras as ae
-from six import text_type
 from flask import Blueprint, request
-from .captcha import check_captcha
-from ckan.lib import captcha
 
 import logging
 
@@ -20,51 +15,9 @@ log = logging.getLogger(__name__)
 advancedauth_user = Blueprint("advancedauth_user", __name__, url_prefix="/user")
 
 
-class ExtendedRequestResetView(RequestResetView):
-    def get(self) -> str:
-        self._prepare()
-        return base.render("user/request_reset.html", {"username": current_user.name})
-
-
-class ExtendedPerformResetView(PerformResetView):
-    # This is a copy of the PerformResetView post method from ckan/views/user.py
-    # Adds update_password_date and changes validation error dict to error_summary
-    def post(self, id):
-        context, user_dict = self._prepare(id)
-        context["reset_password"] = True
-        user_state = user_dict["state"]
-        error_summary = None
-        try:
-            new_password = self._get_form_password()
-            user_dict["password"] = new_password
-            username = request.form.get("name")
-            if username is not None and username != "":
-                user_dict["name"] = username
-            user_dict["reset_key"] = g.reset_key
-            user_dict["state"] = model.State.ACTIVE
-            logic.get_action("user_update")(context, user_dict)
-            mailer.create_reset_key(context["user_obj"])
-            ae.update_password_date(user_dict["id"], "password_last_reset_date")
-            h.flash_success(_("Your password has been reset."))
-            return h.redirect_to("home.index")
-        except logic.NotAuthorized:
-            h.flash_error(_("Unauthorized to edit user %s") % id)
-        except logic.NotFound:
-            h.flash_error(_("User not found"))
-        except dictization_functions.DataError:
-            h.flash_error(_("Integrity Error"))
-        except logic.ValidationError as e:
-            error_summary = e.error_summary
-        except ValueError as e:
-            error_summary = text_type(e)
-        user_dict["state"] = user_state
-        extra_vars = {"user_dict": user_dict, "error_summary": error_summary}
-        return base.render("user/perform_reset.html", extra_vars)
-
-
 class ExtendedEditView(EditView):
-    # This is a copy of the EditView post method from ckan/views/user.py reused for required_reset page
-    # Adds update_password_date
+    # This is a copy of the EditView post method from ckan/views/user.py reused for new user register/profile page
+    # Adds update registration_complete update
     def post(self, id=None):
         context, id = self._prepare(id)
         if not context["save"]:
@@ -134,19 +87,6 @@ class ExtendedEditView(EditView):
         return base.render("user/register.html", extra_vars)
 
 
-class ExtendedRegisterView(RegisterView):
-    # Override original check_recaptcha (google recaptcha) with turnstile check_captcha
-    def post(self):
-        captcha.check_recaptcha = check_captcha
-        return super().post()
-
-
-advancedauth_user.add_url_rule(
-    "/reset/<id>", view_func=ExtendedPerformResetView.as_view(str("perform_reset"))
-)
-advancedauth_user.add_url_rule(
-    "/required_reset/<id>", view_func=ExtendedEditView.as_view(str("required_reset"))
-)
 advancedauth_user.add_url_rule(
     "/register", view_func=ExtendedEditView.as_view(str("register"))
 )
